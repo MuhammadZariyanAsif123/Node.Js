209cea602aaf92d0d9fdc99bd3a592cdd7ff5136
MuhammadZariyanAsif123 <33691797+MuhammadZariyanAsif123@users.noreply.github.com>
2022-11-30 22:43:09 +0500

    1 file modified, 1 added

--- App.js
+++ App.js
@@ -11,6 +11,8 @@ const hostname = '127.0.0.1';
 
 const port = '3000';
 
+let index = fs.readFileSync('./Index.html', 'utf8');
+
 let home = fs.readFileSync('./Home.html', { encoding: 'utf8', flag: 'r' });
 
 let about = fs.readFileSync('./About.html', { encoding: 'utf8', flag: 'r' });
@@ -18,10 +20,11 @@ let about = fs.readFileSync('./About.html', { encoding: 'utf8', flag: 'r' });
 let server = http.createServer((req, res) => {
   let url = req.url;
 
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-    
-  res.end(home);
+  console.log(url);
 
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+
+  res.end(index);
 });
 
 server.listen(port, hostname, () => {
