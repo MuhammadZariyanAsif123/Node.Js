e1180928d2ef4452079cdbb772a41b44ee0014d8
MuhammadZariyanAsif123 <33691797+MuhammadZariyanAsif123@users.noreply.github.com>
2022-11-30 22:19:25 +0500

    1 file modified, 2 added

--- App.js
+++ App.js
@@ -1,15 +1,44 @@
 // Node.js program to demonstrate the
 // fs.readFileSync() method
 
+// Include Http Module
+
+const fs = require('fs');
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+
+const port = '3000';
+
+let home = fs.readFileSync('./Home.html', { encoding: 'utf8', flag: 'r' });
+
+let about = fs.readFileSync('./About.html', { encoding: 'utf8', flag: 'r' });
+
+let server = http.createServer((req, res) => {
+  let url = req.url;
+
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+    
+  res.end(home);
+
+});
+
+server.listen(port, hostname, () => {
+  console.log('Heloo');
+});
+
+/*
+
 // Include fs module
 const fs = require('fs');
 
 // Calling the readFileSync() method
 // to read 'input.txt' file
-let data = fs.readFileSync('./ReadSample.txt', { encoding: 'utf8', flag: 'r' });
 
 data = data.replace('Hi', 'Hello');
 // Display the file data
 console.log(data);
 
 fs.writeFileSync('./WriteSample.txt', data);
+*/
